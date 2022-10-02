a64104e4d3e52e3836bc82abdd91f6d96b7acaeb
lui viton <artem_5511@mail.ru>
2022-10-03 00:14:31 +0300

    Updates

---
+++ precache-manifest.2064de35b027a4a32076d5fba9c8f3aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "168acb6d90fe2278427c",
+    "url": "/homeworks/static/css/main.9380ffe0.chunk.css"
+  },
+  {
+    "revision": "168acb6d90fe2278427c",
+    "url": "/homeworks/static/js/main.168acb6d.chunk.js"
+  },
+  {
+    "revision": "d8a694060ee1f244579f",
+    "url": "/homeworks/static/js/1.d8a69406.chunk.js"
+  },
+  {
+    "revision": "5754b7693ef59a901dd5",
+    "url": "/homeworks/static/js/runtime~main.5754b769.js"
+  },
+  {
+    "revision": "83f855d44ed1f3051d89483f75ec9ab0",
+    "url": "/homeworks/index.html"
+  }
+];
