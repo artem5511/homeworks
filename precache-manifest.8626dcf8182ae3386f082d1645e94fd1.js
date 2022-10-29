1e91c97486a46b878b83da094d0a8296cb3501dc
lui viton <artem_5511@mail.ru>
2022-10-30 00:56:03 +0300

    Updates

---
+++ precache-manifest.8626dcf8182ae3386f082d1645e94fd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "059db61ecb7076c9f67c",
+    "url": "/hws2-master/static/css/main.d9226e2b.chunk.css"
+  },
+  {
+    "revision": "059db61ecb7076c9f67c",
+    "url": "/hws2-master/static/js/main.059db61e.chunk.js"
+  },
+  {
+    "revision": "857a6bd2ccb0d531d141",
+    "url": "/hws2-master/static/js/1.857a6bd2.chunk.js"
+  },
+  {
+    "revision": "fd6da9d72eef290c478e",
+    "url": "/hws2-master/static/js/runtime~main.fd6da9d7.js"
+  },
+  {
+    "revision": "348141525984b318448270566606dc3c",
+    "url": "/hws2-master/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/homeworks/precache-manifest.81fb67e4ee97f8db3c80ecfb216bad99.js"
+  "/hws2-master/precache-manifest.8626dcf8182ae3386f082d1645e94fd1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/homeworks/index.html", {
+workbox.routing.registerNavigationRoute("/hws2-master/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
